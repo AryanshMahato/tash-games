c516616734e24f3659afff3df95bcbffdb6e553c
Aryansh Mahato <aryansh.mahato@sourcefuse.com>
2023-09-23 19:10:53 +0530

    feat(join-game): add join game button

--- src/features/CreateGame/components/useCreateGame.ts
+++ src/features/CreateGame/components/useCreateGame.ts
@@ -2,16 +2,27 @@ import { useForm } from "react-hook-form";
 import { useStore } from "../../../stores/store.ts";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { CreateGameSchema } from "../schema/CreateGame.schema.ts";
+import { useEffect } from "react";
 
 export const useCreateGame = () => {
-  const { name } = useStore();
+  const { name, setName } = useStore();
 
-  const { control, handleSubmit } = useForm({
+  const { control, handleSubmit, watch } = useForm({
     defaultValues: {
       name,
     },
     resolver: zodResolver(CreateGameSchema),
   });
+
+  useEffect(() => {
+    const subscription = watch((data) => {
+      setName(data.name || name);
+    });
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [name, setName, watch]);
 
   const onSubmit = handleSubmit((data, event) => {
     console.log(data, event);
